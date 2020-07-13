445d37179c0db2767b75d91fef4b089c4383cb7f
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2020-07-13 14:30:35 +0200

    Revamp engine without changing functionality

--- www/js/engine.js
+++ www/js/engine.js
@@ -3,8 +3,8 @@ var rules // To hold ruleset in between instructions
 var stats // To keep track of statistics of a single instruction
 var random // To hold an array of random values required for shuffling
 
-// Interaction with main thread
 this.onmessage = (msg) => {
+  // Reset statistics
   stats = {
     games: 0,
     balance: 0,
@@ -16,11 +16,15 @@ this.onmessage = (msg) => {
     lossesAfterDoubling: 0,
     blackjacks: 0,
     surrenders: 0,
-    splits: 0
+    splits: 0,
+    tripleSevenBonuses: 0
   }
+
+  // Read ruleset and create deck or run a given amount of simulations
   if (msg.data.isRuleset) {
     rules = msg.data
     deck = new Array(rules.deckCount * 52)
+    deck.index = 0
     random = new Uint32Array(deck.length)
     for (let card = 2, index = 0; card <= 11; card++) {
       const amount = (card === 10) ? 16 * rules.deckCount : 4 * rules.deckCount
@@ -29,72 +33,20 @@ this.onmessage = (msg) => {
         index++
       }
     }
-    shuffleDeck()
   } else {
-    simulate(msg.data)
+    const count = msg.data
+    const deckThreshold = Math.round(52 * rules.deckCount * (100 - rules.deckPenetration) / 100)
+    for (let i = 0; i < count; i++) {
+      if (deck.index < deckThreshold || rules.shuffleEachGame) {
+        shuffleDeck()
+      }
+      playGame()
+      stats.games++
+    }
   }
+
+  // Send back statistics when instruction is finished
   this.postMessage(stats)
-}
-
-// Simulate a given amount of games
-function simulate (count) {
-  const deckThreshold = ((100 - rules.deckPenetration) / 100) * rules.deckCount * 52
-  for (let i = 0; i < count; i++) {
-    playGame()
-    stats.games++
-    if (deck.index < deckThreshold || rules.shuffleEachGame) {
-      shuffleDeck()
-    }
-  }
-}
-
-// Play a single full game
-function playGame () {
-  var playerStartingHand = []
-  var dealerStartingHand = []
-  playerStartingHand.push(takeCard())
-  dealerStartingHand.push(takeCard())
-  playerStartingHand.push(takeCard())
-  dealerStartingHand.push(takeCard())
-  if (handValue(playerStartingHand) === 21) {
-    if (handValue(dealerStartingHand) === 21) {
-      stats.pushesNormal++
-      return
-    } else {
-      stats.blackjacks++
-      stats.balance += rules.blackjackPayout
-      return
-    }
-  }
-  var playerFinalHands = playerTurn(playerStartingHand, dealerStartingHand[0])
-  if (playerFinalHands === 'surrender') {
-    if (rules.earlySurrender || !(handValue(dealerStartingHand) === 21)) {
-      stats.surrenders++
-      stats.balance -= 0.5
-      return
-    } else {
-      stats.lossesNormal++
-      stats.balance -= 1
-      return
-    }
-  }
-  var dealerFinalHand = dealerTurn(dealerStartingHand)
-  if (rules.tripleSevenBonus && playerFinalHands[0][0] === 7 && playerFinalHands[0][1] === 7 && playerFinalHands[0][2] === 7 && typeof (playerFinalHands[1]) === 'undefined') {
-    stats.balance += 1
-  }
-  for (const hand of playerFinalHands) {
-    stats.balance += calculateWinner(handValue(hand), hand.doubled, handValue(dealerFinalHand))
-  }
-}
-
-// Count the total value of a hand
-function handValue (hand) {
-  return hand.reduce((a, b) => a + b)
-}
-
-// Count the amount of aces in a hand
-function aceCount (hand) {
-  return hand.reduce((a, b) => (b === 11 ? a + 1 : a), 0)
 }
 
 // Shuffle the global deck using the Fisher-Yates algorithm and Crypto web API
@@ -112,13 +64,152 @@ function takeCard () {
   return deck[deck.index--]
 }
 
+// Count the total value of a hand
+function handValue (hand) {
+  return hand.reduce((a, b) => (a + b), 0)
+}
+
+// Count the amount of aces in a hand
+function aceCount (hand) {
+  return hand.reduce((a, b) => (b === 11 ? a + 1 : a), 0)
+}
+
+// Play a single full round/game
+function playGame () {
+  const playerStartingHand = []
+  const dealerStartingHand = []
+
+  // Deal initial cards
+  playerStartingHand.push(takeCard())
+  dealerStartingHand.push(takeCard())
+  playerStartingHand.push(takeCard())
+  dealerStartingHand.push(takeCard())
+
+  // Calculate hand values
+  const playerStartingHandValue = handValue(playerStartingHand)
+  const dealerStartingHandValue = handValue(dealerStartingHand)
+
+  // Check for blackjack(s)
+  if (playerStartingHandValue === 21) {
+    if (dealerStartingHandValue === 21) {
+      stats.pushesNormal++
+    } else {
+      stats.blackjacks++
+      stats.balance += rules.blackjackPayout
+    }
+    return
+  }
+
+  // Variables for the player's turn
+  const playerFinalHands = []
+  var splitAllowed = true
+  var splitCount = 0
+
+  // Simulate the player's turn, recursing when splitting
+  function playerTurn (hand, isFirstHand) {
+    const result = playHand(hand, splitAllowed, isFirstHand, dealerStartingHand[0])
+    if (result === null) {
+      if (rules.earlySurrender || !(dealerStartingHandValue === 21)) {
+        stats.surrenders++
+        stats.balance -= 0.5
+      } else {
+        stats.lossesNormal++
+        stats.balance -= 1
+      }
+      return 0
+    } else if (result.split) {
+      stats.splits++
+      splitCount++
+      splitAllowed = !(splitCount >= rules.maxSplits)
+      playerTurn([result[0], takeCard()], false)
+      playerTurn([result[1], takeCard()], false)
+    } else {
+      playerFinalHands.push(result)
+    }
+  }
+
+  // Start the player's turn and catch a surrender
+  if (playerTurn(playerStartingHand, true) === 0) {
+    return
+  }
+
+  // Simulate the dealer's turn
+  const dealerFinalHand = dealerStartingHand
+  while (true) {
+    if (handValue(dealerFinalHand) < 17) {
+      dealerFinalHand.push(takeCard())
+      continue
+    }
+    if (rules.dealerHitsSoft17 && handValue(dealerFinalHand) === 17 && aceCount(dealerFinalHand) === 1) {
+      dealerFinalHand.push(takeCard())
+      continue
+    }
+    if (handValue(dealerFinalHand) > 21 && aceCount(dealerFinalHand) > 0) {
+      dealerFinalHand[dealerFinalHand.indexOf(11)] = 1
+      continue
+    }
+    break
+  }
+  const dealerFinalHandValue = handValue(dealerFinalHand)
+
+  // Triple seven bonus logic
+  if (rules.tripleSevenBonus && playerFinalHands.length === 1 && playerFinalHands[0][0] === 7 && playerFinalHands[0][1] === 7 && playerFinalHands[0][2] === 7) {
+    stats.tripleSevenBonuses++
+    stats.balance += 1
+  }
+
+  // Calculate winner of each hand
+  for (const hand of playerFinalHands) {
+    const playerHandValue = handValue(hand)
+    if (playerHandValue > 21) {
+      if (hand.doubled) {
+        stats.lossesAfterDoubling++
+        stats.balance -= 2
+      } else {
+        stats.lossesNormal++
+        stats.balance -= 1
+      }
+    } else if (dealerFinalHandValue > 21) {
+      if (hand.doubled) {
+        stats.winsAfterDoubling++
+        stats.balance += 2
+      } else {
+        stats.winsNormal++
+        stats.balance += 1
+      }
+    } else if (dealerFinalHandValue === playerHandValue) {
+      if (hand.doubled) {
+        stats.pushesAfterDoubling++
+      } else {
+        stats.pushesNormal++
+      }
+    } else if (dealerFinalHandValue > playerHandValue) {
+      if (hand.doubled) {
+        stats.lossesAfterDoubling++
+        stats.balance -= 2
+      } else {
+        stats.lossesNormal++
+        stats.balance -= 1
+      }
+    } else if (dealerFinalHandValue < playerHandValue) {
+      if (hand.doubled) {
+        stats.winsAfterDoubling++
+        stats.balance += 2
+      } else {
+        stats.winsNormal++
+        stats.balance += 1
+      }
+    }
+  }
+}
+
 // Simulate the player's actions on a given hand
-function playHand (hand, splitAvailable, surrenderAvailable, resultOfSplit, dealerUpcard) {
+function playHand (hand, splitAvailable, isFirstHand, dealerUpcard) {
   hand.doubled = false
   hand.split = false
   var skipWhile = false
   var move
-  if (resultOfSplit && !rules.allowHittingSplitAces && hand[0] === 11 && !(hand[0] === 11 && hand[1] === 11 && rules.allowResplittingAces)) {
+  if (!isFirstHand && !rules.allowHittingSplitAces && hand[0] === 11 && !(hand[0] === 11 && hand[1] === 11 && rules.allowResplittingAces)) {
     if (handValue(hand) > 21) {
       hand[0] = 1
     }
@@ -136,14 +227,14 @@ function playHand (hand, splitAvailable, surrenderAvailable, resultOfSplit, deal
       move = rules.hardStrategy[handValue(hand)][dealerUpcard]
     }
     if (move.charAt(0) === 'R') {
-      if (rules.allowSurrender && surrenderAvailable && hand.length === 2) {
-        return 'surrender'
+      if (rules.allowSurrender && isFirstHand && hand.length === 2) {
+        return
       } else {
         move = move.charAt(1).toUpperCase()
       }
     }
     if (move.charAt(0) === 'D') {
-      if (!rules.allowDouble || hand.length > 2 || (!rules.allowDoubleAfterSplit && resultOfSplit) || (rules.doubleRestricted && !(handValue(hand) === 9) && !(handValue(hand) === 10) && !(handValue(hand) === 11))) {
+      if (!rules.allowDouble || hand.length > 2 || (!rules.allowDoubleAfterSplit && !isFirstHand) || (rules.doubleRestricted && !(handValue(hand) === 9) && !(handValue(hand) === 10) && !(handValue(hand) === 11))) {
         move = move.charAt(1).toUpperCase()
       } else {
         hand.push(takeCard())
@@ -156,7 +247,6 @@ function playHand (hand, splitAvailable, surrenderAvailable, resultOfSplit, deal
         move = 'H'
       } else {
         if (splitAvailable && rules.allowSplitting) {
-          stats.splits += 1
           hand.split = true
           return hand
         } else {
@@ -178,104 +268,4 @@ function playHand (hand, splitAvailable, surrenderAvailable, resultOfSplit, deal
     }
   }
   return hand
-}
-
-// Play the player's turn
-function playerTurn (startingHand, dealerUpcard) {
-  var cumulativeHands = []
-  var splitCount = 0
-  var splitAllowed = true
-  var firstPlayResult = playHand(startingHand, splitAllowed, true, false, dealerUpcard)
-  if (firstPlayResult === 'surrender') {
-    return 'surrender'
-  } else if (firstPlayResult.split) {
-    if (firstPlayResult === [11, 11] && !rules.allowResplittingAces) {
-      splitAllowed = false
-    }
-    splitCount++
-    split(firstPlayResult)
-  } else {
-    cumulativeHands.push(firstPlayResult)
-  }
-  function split (source) {
-    if (splitCount >= rules.maxSplits && !(rules.maxSplits === 0)) {
-      splitAllowed = false
-    }
-    for (let i = 0; i < 2; i++) {
-      var hand = [source[i], takeCard()]
-      var result = playHand(hand, splitAllowed, false, true, dealerUpcard)
-      if (result.split) {
-        splitCount++
-        split(result)
-      } else {
-        cumulativeHands.push(result)
-      }
-    }
-  }
-  return cumulativeHands
-}
-
-// Play out the dealers hand
-function dealerTurn (hand) {
-  while (true) {
-    if (handValue(hand) < 17) {
-      hand.push(takeCard())
-      continue
-    }
-    if (rules.dealerHitsSoft17 && handValue(hand) === 17 && aceCount(hand) === 1) {
-      hand.push(takeCard())
-      continue
-    }
-    if (handValue(hand) > 21 && aceCount(hand) > 0) {
-      hand[hand.indexOf(11)] = 1
-      continue
-    }
-    break
-  }
-  return hand
-}
-
-// Calculate the winner based on the players and the dealers hands
-function calculateWinner (playerValue, doubled, dealerValue) {
-  if (playerValue > 21) {
-    if (doubled) {
-      stats.lossesAfterDoubling++
-      return -2
-    } else {
-      stats.lossesNormal++
-      return -1
-    }
-  } else if (dealerValue > 21) {
-    if (doubled) {
-      stats.winsAfterDoubling++
-      return 2
-    } else {
-      stats.winsNormal++
-      return 1
-    }
-  } else if (dealerValue === playerValue) {
-    if (doubled) {
-      stats.pushesAfterDoubling++
-      return 0
-    } else {
-      stats.pushesNormal++
-      return 0
-    }
-  } else if (dealerValue > playerValue) {
-    if (doubled) {
-      stats.lossesAfterDoubling++
-      return -2
-    } else {
-      stats.lossesNormal++
-      return -1
-    }
-  } else if (dealerValue < playerValue) {
-    if (doubled) {
-      stats.winsAfterDoubling++
-      return 2
-    } else {
-      stats.winsNormal++
-      return 1
-    }
-  }
 }
