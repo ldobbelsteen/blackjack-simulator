6bbe94a1f266cd23546b47e45c771534ddddc9ec
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2024-05-08 16:54:01 +0200

    Fix crypto

--- src/js/blackjack/shuffle.ts
+++ src/js/blackjack/shuffle.ts
@@ -17,7 +17,7 @@ export class Shuffler {
 
   /** Fill the entropy array with new randomness */
   private fetchEntropy(): void {
-    window.crypto.getRandomValues(this.entropy);
+    crypto.getRandomValues(this.entropy);
     this.index = 0;
   }
 
